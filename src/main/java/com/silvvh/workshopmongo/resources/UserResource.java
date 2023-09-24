ca41a54a1c47a4f9e9010166e98ee11675ba5ed4
Victor Brito <victorcyty@hotmail.com>
2023-09-24 15:31:50 -0300

    UserDTO and findAll

--- src/main/java/com/silvvh/workshopmongo/resources/UserResource.java
+++ src/main/java/com/silvvh/workshopmongo/resources/UserResource.java
@@ -2,12 +2,15 @@ package com.silvvh.workshopmongo.resources;
 
 import com.silvvh.workshopmongo.domain.User;
 import com.silvvh.workshopmongo.service.UserService;
+import dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,7 +20,9 @@ public class UserResource {
     @Autowired
     private UserService service;
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok().body(service.findAll());
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<UserDTO> usersDTO = service.findAll().stream().map(u -> new UserDTO(u)).toList();
+        return ResponseEntity.ok().body(usersDTO);
     }
+
 }
---
+++ src/main/java/dto/UserDTO.java
@@ -0,0 +1,48 @@
+package dto;
+
+import com.silvvh.workshopmongo.domain.User;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    @Serial
+    private final static long serialVersionUID = 1L;
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
