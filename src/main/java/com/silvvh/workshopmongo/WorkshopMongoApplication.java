bc3ee5d849228aeb6399595af89c7853e38fafc5
Victor Brito <victorcyty@hotmail.com>
2023-09-24 12:25:29 -0300

    First commit

---
+++ src/main/java/com/silvvh/workshopmongo/WorkshopMongoApplication.java
@@ -0,0 +1,13 @@
+package com.silvvh.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silvvh/workshopmongo/WorkshopMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvvh.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
