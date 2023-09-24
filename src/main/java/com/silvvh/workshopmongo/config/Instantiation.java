227b369b933d3bb6beb6ffbb3217e30785ff95ae
Victor Brito <victorcyty@hotmail.com>
2023-09-24 19:05:24 -0300

    User posts reference

--- src/main/java/com/silvvh/workshopmongo/config/Instantiation.java
+++ src/main/java/com/silvvh/workshopmongo/config/Instantiation.java
@@ -4,14 +4,11 @@ import com.silvvh.workshopmongo.domain.Post;
 import com.silvvh.workshopmongo.domain.User;
 import com.silvvh.workshopmongo.repository.PostRepository;
 import com.silvvh.workshopmongo.repository.UserRepository;
-import com.silvvh.workshopmongo.service.UserService;
 import dto.AuthorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.objenesis.instantiator.annotations.Instantiator;
 
-import java.time.Instant;
 import java.time.LocalDate;
 import java.util.Arrays;
 
@@ -24,7 +21,7 @@ public class Instantiation implements CommandLineRunner {
     private PostRepository postRepository;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args)  {
 
         userRepository.deleteAll();
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
@@ -32,7 +29,9 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
         Post post1 = new Post(null, LocalDate.of(2023, 4, 21), "Partiu viagem", "Vou para São Paulo", new AuthorDTO(maria));
-        Post pos2 = new Post(null, LocalDate.of(2023, 12, 20), "Enfim férias", "Bora de aproveitar o descanso!", new AuthorDTO(maria));
-        postRepository.saveAll(Arrays.asList(post1, pos2));
+        Post post2 = new Post(null, LocalDate.of(2023, 12, 20), "Enfim férias", "Bora de aproveitar o descanso!", new AuthorDTO(maria));
+        postRepository.saveAll(Arrays.asList(post1, post2));
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
